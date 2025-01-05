285c62874b3d65b9f89fda15c77e0f9d3e579c10
yarovikov <alex.yarovikov@gmail.com>
2025-01-05 17:53:52 +0300

    Panels

---
+++ src/components/panel-options.js
@@ -0,0 +1,45 @@
+import {Spinner} from '@wordpress/components';
+import apiFetch from '@wordpress/api-fetch';
+import {useState, useEffect} from '@wordpress/element';
+import {useDispatch} from '@wordpress/data';
+import BlockComponents from "./block-components";
+
+export default function PanelOptions({name}) {
+
+  const [data, setData] = useState([]);
+  const {editPost} = useDispatch('core/editor');
+
+  const onChangeAttribute = (id = null, key, value, metaName = true) => {
+    editPost({meta: {[metaName]: value}})
+  };
+
+  useEffect(() => {
+    apiFetch({path: `/${name}/v1/data`})
+      .then((res) => {
+        setData(res)
+        res.options.map((option) => {
+          [option.name] in option && onChangeAttribute([option.name], option.value);
+        });
+      })
+      .catch((error) => console.error(error?.message));
+  }, []);
+
+  return (
+    <>
+      {!data && <Spinner/>}
+      {data && data.options && data.options.length > 0 && (
+        data.options.map((section, index) => {
+          return (
+            <div key={index} className='gutengood gutengood-options'>
+              <BlockComponents
+                attributes={section.fields}
+                components={section.fields}
+                onChange={onChangeAttribute}
+              />
+            </div>
+          );
+        })
+      )}
+    </>
+  )
+}
